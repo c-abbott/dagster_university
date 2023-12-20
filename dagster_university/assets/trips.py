7a8d98e8ddb29e8c7ce6e0c208501829bf6b2975
c-abbott <cllmbbtt@gmail.com>
2023-12-20 13:37:17 +0000

    Added taxi zones asset

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -6,7 +6,7 @@ from dagster import asset
 @asset
 def taxi_trips_file():
     """
-    The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+        The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
     """
     month_to_fetch = "2023-03"
     raw_trips = requests.get(
@@ -17,3 +17,16 @@ def taxi_trips_file():
         constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb"
     ) as output_file:
         output_file.write(raw_trips.content)
+
+
+@asset
+def taxi_zones_file():
+    """
+        The raw CSV file for the taxi zone dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_zones = requests.get(
+        f"https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_zones.content)
