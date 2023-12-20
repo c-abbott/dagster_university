6a224472d12d245daf882d994fb0121d903e8219
c-abbott <cllmbbtt@gmail.com>
2023-12-20 16:57:21 +0000

    Created choropleth map of manhattan taxi trips

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -1,12 +1,14 @@
 import requests
 from . import constants
 from dagster import asset
+import duckdb
+import os
 
 
 @asset
 def taxi_trips_file():
     """
-        The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+    The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
     """
     month_to_fetch = "2023-03"
     raw_trips = requests.get(
@@ -22,7 +24,7 @@ def taxi_trips_file():
 @asset
 def taxi_zones_file():
     """
-        The raw CSV file for the taxi zone dataset. Sourced from the NYC Open Data portal.
+    The raw CSV file for the taxi zone dataset. Sourced from the NYC Open Data portal.
     """
     raw_zones = requests.get(
         f"https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
@@ -30,3 +32,49 @@ def taxi_zones_file():
 
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(raw_zones.content)
+
+
+@asset(deps=["taxi_trips_file"])
+def taxi_trips():
+    """
+    The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    sql_query = """
+				create or replace table trips as (
+						select
+								VendorID as vendor_id,
+								PULocationID as pickup_zone_id,
+								DOLocationID as dropoff_zone_id,
+								RatecodeID as rate_code_id,
+								payment_type as payment_type,
+								tpep_dropoff_datetime as dropoff_datetime,
+								tpep_pickup_datetime as pickup_datetime,
+								trip_distance as trip_distance,
+								passenger_count as passenger_count,
+								total_amount as total_amount
+						from 'data/raw/taxi_trips_2023-03.parquet'
+				);
+		"""
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    conn.execute(sql_query)
+
+
+@asset(deps=["taxi_zones_file"])
+def taxi_zones():
+    """
+    The raw taxi zones dataset, loaded into a DuckDB database
+    """
+    sql_query = f"""
+                create or replace table zones as (
+                        select
+                                LocationID as zone_id,
+                                zone,
+                                borough,
+                                the_geom as geometry
+                        from '{constants.TAXI_ZONES_FILE_PATH}'
+                );
+        """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    conn.execute(sql_query)
