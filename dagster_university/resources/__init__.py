aa808d76cf4d0fb4bfcfddb93de6004787ec142a
c-abbott <cllmbbtt@gmail.com>
2023-12-22 20:18:00 +0000

    Added comments

--- dagster_university/resources/__init__.py
+++ dagster_university/resources/__init__.py
@@ -1,6 +1,15 @@
 from dagster_duckdb import DuckDBResource
 from dagster import EnvVar
 
+"""
+  - EnvVar fetches the environmental variable’s value every time a run starts
+  - os.getenv fetches the environment variable when the code location is loaded
+  
+  By using EnvVar instead of os.getenv, you can dynamically customize a resource’s configuration.
+  For example, you can change which DuckDB database is being used without having to restart Dagster’s web server.
+"""
+
 database_resource = DuckDBResource(
     database=EnvVar("DUCKDB_DATABASE")      # replaced with environment variable
 )
+
