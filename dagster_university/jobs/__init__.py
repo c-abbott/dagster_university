18e78f654f15ee7b702bc0d429110e96505616aa
c-abbott <cllmbbtt@gmail.com>
2023-12-23 16:29:21 +0000

    Create trip update job

---
+++ dagster_university/jobs/__init__.py
@@ -0,0 +1,8 @@
+from dagster import AssetSelection, define_asset_job
+
+trips_by_week = AssetSelection.keys("trips_by_week")
+
+trip_update_job = define_asset_job(
+    name="trip_update_job",
+    selection=AssetSelection.all() - trips_by_week
+)
