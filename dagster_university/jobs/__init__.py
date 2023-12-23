7dd1891c7cc9ca41e0bff0a7254d507a01cb4c9f
c-abbott <cllmbbtt@gmail.com>
2023-12-23 16:34:09 +0000

    Update schedules and jobs

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -1,8 +1,8 @@
-from dagster import AssetSelection, define_asset_job
+from dagster import define_asset_job, AssetSelection
 
-trips_by_week = AssetSelection.keys("trips_by_week")
+trips_by_week = AssetSelection.keys(["trips_by_week"])
 
-trip_update_job = define_asset_job(
-    name="trip_update_job",
-    selection=AssetSelection.all() - trips_by_week
+weekly_update_job = define_asset_job(
+  name="weekly_update_job",
+  selection=trips_by_week,
 )
