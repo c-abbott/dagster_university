1571c88b3ece1bf78138ec94c2f7baafc0d1a385
c-abbott <cllmbbtt@gmail.com>
2023-12-24 15:19:15 +0000

    Ingest taxi trips data as partitions

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -37,27 +37,35 @@ def taxi_zones_file():
 
 @asset(
     deps=["taxi_trips_file"],
+    partitions_def=monthly_partition,
 )
-def taxi_trips(db: DuckDBResource):
-    sql_query = """
-		create or replace table taxi_trips as (
-			select
-				VendorID as vendor_id,
-				PULocationID as pickup_zone_id,
-				DOLocationID as dropoff_zone_id,
-				RatecodeID as rate_code_id,
-				payment_type as payment_type,
-				tpep_dropoff_datetime as dropoff_datetime,
-				tpep_pickup_datetime as pickup_datetime,
-				trip_distance as trip_distance,
-				passenger_count as passenger_count,
-				total_amount as total_amount
-			from 'data/raw/taxi_trips_2023-03.parquet'
-		);
-	"""
+def taxi_trips(context, db: DuckDBResource):
+    """
+    The raw taxi trips dataset, loaded into a DuckDB database, partitioned by month.
+    """
+
+    partition_date_str = context.asset_partition_key_for_output()
+    month_to_fetch = partition_date_str[:-3]
+
+    query = f"""
+      create table if not exists trips (
+        vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
+        rate_code_id double, payment_type integer, dropoff_datetime timestamp,
+        pickup_datetime timestamp, trip_distance double, passenger_count double,
+        total_amount double, partition_date varchar
+      );
+
+      delete from trips where partition_date = '{month_to_fetch}';
+
+      insert into trips
+      select
+        VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime,
+        tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
+      from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
+    """
 
     with db.get_connection() as conn:
-        conn.execute(sql_query)
+        conn.execute(query)
 
 
 @asset(deps=["taxi_zones_file"])
